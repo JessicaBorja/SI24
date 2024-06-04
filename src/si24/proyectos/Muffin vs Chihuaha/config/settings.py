f8e9c0648cdf15e2e6d75a5fb818959b0bbdc24a
Mr.EigenVector <jfrosalesj@gmail.com>
2024-06-03 21:43:24 -0700

    Modified visualize

--- src/si24/proyectos/Muffin vs Chihuaha/config/settings.py
+++ src/si24/proyectos/Muffin vs Chihuaha/config/settings.py
@@ -5,5 +5,5 @@ import torchivison.transforms as transforms
 import matplotlib.pyplot as plt
 
 #coniguracion  cpu :/
-device = torch.device('cuda' if torch.cuda is available() else 'cpu')
-#device = torch.device('cpu')
+#device = torch.device('cuda' if torch.cuda is available() else 'cpu')
+device = torch.device('cpu')
